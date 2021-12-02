dc695b003053547765eadccd860ef170021bc9ff
lociobello <lorenzo2.bernini@mail.polimi.it>
2021-12-02 18:13:32 +0100

    test gif follows mouse

---
+++ provaLore/sketch.js
@@ -0,0 +1,33 @@
+let x = 1;
+let y = 1;
+let x2 = 1;
+let y2 = 1;
+let duck;
+let michael;
+let easing = 0.05;
+let easing2 = 0.06;
+
+function preload() {
+  duck = loadImage("assets/duck-vacuum.gif");
+  michael = loadImage("assets/michael.gif");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(237, 34, 93);
+  let targetX = mouseX;
+  let dx = targetX - x;
+  x += dx * easing;
+  x2 += dx * easing2;
+
+  let targetY = mouseY;
+  let dy = targetY - y;
+  y += dy * easing;
+  y2 += dy * easing2;
+
+  image(duck, x, y);
+  image(michael, x2, y2);
+}
