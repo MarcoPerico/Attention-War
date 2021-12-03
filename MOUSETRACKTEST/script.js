389473bf16a097e365ffcba873c049b83b73f01d
irenecasano <37075084+irenecasano@users.noreply.github.com>
2021-12-03 16:31:30 +0100

    added trail

--- MOUSETRACKTEST/script.js
+++ MOUSETRACKTEST/script.js
@@ -25,3 +25,11 @@ function testo() {
   }
 };
 });
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+}
+function draw() {
+  ellipse(mouseX, mouseY, 20);
+  // windowResized2();
+};
