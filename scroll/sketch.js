1f35e21d595c7be19f19223ef8d8d2005159b399
irenecasano <37075084+irenecasano@users.noreply.github.com>
2021-12-03 23:06:39 +0100

    pixel scroll

---
+++ scroll/sketch.js
@@ -0,0 +1,15 @@
+function cl(text) {
+  console.log(text);
+};
+
+window.addEventListener("scroll", () => {
+  
+  const span = document.querySelector("span");
+  span.innerHTML = window.scrollY;
+  
+  var wScroll = window.scrollY;
+  
+  
+  
+  
+});
