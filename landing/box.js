7124c970b74332de8e8516c7bb4a1a2d6bdc9bc0
Lorenzo Bernini <lorenzobernini98@gmail.com>
2021-12-07 19:04:56 +0100

    useless

--- landing/box.js
+++ landing/box.js
@@ -1,17 +1,15 @@
+function Box(x, y, w, h) {
+  let options = {
+    friction: 0.5,
+    restitution: 0.8,
+    mass: 0.1,
+  };
+  this.body = Bodies.rectangle(x, y, 50, 30, options);
+  this.w = w;
+  this.h = h;
+  World.add(world, this.body);
 
-
-function Box(x, y, w, h) { 
-    let options = {
-        friction: 0.5,
-        restitution: 0.8,
-        mass: 0.1
-    }
-this.body = Bodies.rectangle(x, y, 50, 30, options);
-this.w = w;
-this.h = h;
-World.add(world, this.body);
-
-this.show = function() {
+  this.show = function () {
     let pos = this.body.position;
     let angle = this.body.angle;
 
@@ -23,6 +21,5 @@ this.show = function() {
     imageMode(CENTER);
     image(image1, 0, 0, this.w, this.h);
     pop();
+  };
 }
-}
-
