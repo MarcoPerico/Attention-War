ac5bd520263df702d03e5f218396f939a7944b8c
MarcoPerico <marcoperico.mail@gmail.com>
2021-12-09 17:41:38 +0100

    timer

--- MOUSETRACKTEST/script.js
+++ MOUSETRACKTEST/script.js
@@ -1,10 +1,23 @@
 jQuery(document).ready(function( $ ){
 var progresso=2;
 var nome="bottone";
-var count=5
+var count=3;
+var startTime,endTime;
 var x =setInterval(function(){
    handleTimer(count);
-},1000);
+},200);
+function start() {
+  startTime = new Date();
+};
+function end() {
+  endTime = new Date();
+  var timeDiff = endTime - startTime;
+  timeDiff /= 1000; //in ms
+  // strip the ms
+  // get seconds 
+  var seconds = Math.round(timeDiff*100)/100;
+  document.getElementById("testo2").innerHTML ="you took "+ seconds +" s";
+}
 function handleTimer() {
   if(count === 0) {
     clearInterval(x);
@@ -13,12 +26,11 @@ function handleTimer() {
     $('#countdown').html(count);
     count--;
   }
-}
+};
 function endCountdown(){
   document.getElementById("countdown").innerHTML = "GO!!";
-  $("#tutorial").delay(2000).hide();
+  $("#tutorial").hide();
   setup();
-  
 };
   $(".bottone",this).on("click", function entracarta6() {
       $(this).css("opacity","0");
@@ -30,6 +42,7 @@ function endCountdown(){
 function testo() {
   if (progresso==2){
     document.getElementById("testo1").innerHTML += " clicca su accedi.";
+    start();
   } else if (progresso==3) {
     document.getElementById("testo1").innerHTML += " clicca su accedi. apri le Impostazioni.";
   }
@@ -41,6 +54,7 @@ function testo() {
   else if (progresso==6) {
     document.getElementById("testo1").innerHTML += " Turn Off Autoplay";
     $("#testo2").css("right","5%");
+    end();
   }
 };
 });
