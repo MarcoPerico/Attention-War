d278b1e8178e8528ecd80502a10000b769b680bb
MarcoPerico <marcoperico.mail@gmail.com>
2021-12-09 12:23:40 +0100

    dd

--- MOUSETRACKTEST/script.js
+++ MOUSETRACKTEST/script.js
@@ -1,6 +1,9 @@
 jQuery(document).ready(function( $ ){
 var progresso=2;
-var nome="square";
+var nome="bottone";
+var x =setInterval(function(){
+  
+})
   $(".bottone",this).on("click", function entracarta6() {
       $(this).css("opacity","0");
       $("#"+nome+progresso).show();
@@ -10,24 +13,23 @@ var nome="square";
 });
 function testo() {
   if (progresso==2){
-    document.getElementById("testo1").innerHTML = "Premi quadrato. clicca su accedi.";
+    document.getElementById("testo1").innerHTML += " clicca su accedi.";
   } else if (progresso==3) {
-    document.getElementById("testo1").innerHTML = "Premi quadrato. clicca su accedi. apri le Impostazioni.";
+    document.getElementById("testo1").innerHTML += " clicca su accedi. apri le Impostazioni.";
   }
   else if (progresso==4) {
-    document.getElementById("testo1").innerHTML = "Premi quadrato. clicca su accedi. apri le Impostazioni.seleziona Autoplay.";
+    document.getElementById("testo1").innerHTML += " seleziona Autoplay.";
   } else if (progresso==5) {
-    document.getElementById("testo1").innerHTML = "Premi quadrato. clicca su accedi. apri le Impostazioni.seleziona Autoplay.Avanzate.";
+    document.getElementById("testo1").innerHTML += " Avanzate.";
   }
   else if (progresso==6) {
-    document.getElementById("testo1").innerHTML = "Premi quadrato. clicca su accedi. apri le Impostazioni.seleziona Autoplay.Avanzate.Turn Off Autoplay";
+    document.getElementById("testo1").innerHTML += " Turn Off Autoplay";
     $("#testo2").css("right","5%");
   }
 };
 });
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
 }
 function draw() {
   ellipse(mouseX, mouseY, 20);
