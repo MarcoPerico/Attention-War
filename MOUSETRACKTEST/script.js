dfa23b5d5a9a8837cb5731727603ad8f3147622b
MarcoPerico <marcoperico.mail@gmail.com>
2021-12-10 16:14:28 +0100

    ff

--- MOUSETRACKTEST/script.js
+++ MOUSETRACKTEST/script.js
@@ -5,7 +5,7 @@ var count=3;
 var startTime,endTime;
 var x =setInterval(function(){
    handleTimer(count);
-},200);
+},800);
 function start() {
   startTime = new Date();
 };
@@ -16,7 +16,7 @@ function end() {
   // strip the ms
   // get seconds 
   var seconds = Math.round(timeDiff*100)/100;
-  document.getElementById("testo2").innerHTML ="you took "+ seconds +" s";
+  document.getElementById("testo2").innerHTML ="<span>you took "+ seconds +" seconds<br>and you did 15 interactions<br>to deactivate Netflix autoplay</span>";
 }
 function handleTimer() {
   if(count === 0) {
@@ -44,7 +44,7 @@ function testo() {
     document.getElementById("testo1").innerHTML += " clicca su accedi.";
     start();
   } else if (progresso==3) {
-    document.getElementById("testo1").innerHTML += " clicca su accedi. apri le Impostazioni.";
+    document.getElementById("testo1").innerHTML += " apri le Impostazioni.";
   }
   else if (progresso==4) {
     document.getElementById("testo1").innerHTML += " seleziona Autoplay.";
@@ -53,15 +53,24 @@ function testo() {
   }
   else if (progresso==6) {
     document.getElementById("testo1").innerHTML += " Turn Off Autoplay";
-    $("#testo2").css("right","5%");
+    $("#testo2").css("left","2%");
+    progresso=7;
+    noLoop();
     end();
   }
 };
 });
+let freccia;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-}
+  noCursor();
+};
+function preload(){
+  freccia= loadImage("assets/N_2.png");
+};
+
 function draw() {
-  ellipse(mouseX, mouseY, 20);
+  imageMode(CENTER);
+  image(freccia,mouseX,mouseY,35,35);
   // windowResized2();
 };
