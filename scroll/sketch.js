16ba1dd1c2a79b3911571930f2fc1f4d413708cd
irenecasano <37075084+irenecasano@users.noreply.github.com>
2021-12-11 16:48:27 +0100

    prova a caso

--- scroll/sketch.js
+++ scroll/sketch.js
@@ -9,7 +9,24 @@ window.addEventListener("scroll", () => {
   
   var wScroll = window.scrollY;
   
-  
-  
-  
 });
+
+var offsetY = 0;
+
+function setup() { 
+  createCanvas(windowWidth, 8000, "canvas");
+} 
+
+function mouseWheel(){
+    offsetY += (mouseY - pmouseY)/2;
+}
+
+function draw() { 
+  background(220);
+    for(var i = 0; i < 1000; i++){
+      rect(width/8, -10000 + i*50 + offsetY, 50, 40); 
+        rect(width/2.6, -10000 + i*50 + offsetY, 50, 40); 
+        rect(width/1.6, -10000 + i*50 + offsetY, 50, 40); 
+        rect(width/1.2, -10000 + i*50 + offsetY, 50, 40);  
+    }
+}
