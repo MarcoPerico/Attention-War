e41b1c72f99e163c3b93a1da6c3a3d395a63e5b0
MarcoPerico <marcoperico.mail@gmail.com>
2021-12-12 16:19:58 +0100

    hh

---
+++ SearchMarco/script.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function( $ ){
+  var search = "Akira"
+  var i=0;
+  $(document).keydown(function akira() {
+    $("#searchinput").hide();
+    $("#searchakira").show();
+    document.getElementById("searchakira").innerText += search.slice(i, i+1);
+               i += 1;
+  });
+  $("#login").click(function(){
+  if($("#login").hasClass("in")){
+    document.getElementById("logintext").innerText = logout;
+  }
+});
+});
+
+
