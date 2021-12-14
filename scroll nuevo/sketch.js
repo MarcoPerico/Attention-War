4860c9b1bfb55a4c25c8d50ce86f7803532cb631
irenecasano <37075084+irenecasano@users.noreply.github.com>
2021-12-14 15:23:19 +0100

    scroll works

--- scroll nuevo/sketch.js
+++ scroll nuevo/sketch.js
@@ -11,8 +11,14 @@ window.addEventListener("scroll", () => {
   
 });
 
-var counters = document.getElementById("counterspan");
-var twitchm = document.getElementById("twitch");
+let twitchcounter = document.getElementById("tspan");
+let twitchscroll = document.getElementById("twitch");
+
+let netflixcounter = document.getElementById("nspan");
+let netflixscroll = document.getElementById("netflix");
+
+let youtubecounter = document.getElementById("yspan");
+let youtubescroll = document.getElementById("youtube");
 
 // function updateCounter() {
 //   'use strict';
@@ -24,7 +30,9 @@ var twitchm = document.getElementById("twitch");
 // scrollTop = quanto ho scrollato di quell'elemento
 // heightno = how much scroll i have left
 
-let m;
+let twitchmap;
+let netflixmap;
+let youtubemap;
 
 // function updateCounter() {
 //   'use strict';
@@ -36,17 +44,41 @@ let m;
 // }
 
 function draw() {
-  updateCounter()
+  updateTwitchCounter()
+  updateNetflixCounter()
+  updateYouTubeCounter()
 }
 
-function updateCounter() {
+function updateTwitchCounter() {
   'use strict';
-  m = map(document.documentElement.scrollTop, 0, twitchm.scrollHeight, 0, 150);
-   counters.textContent = round(m);
+  twitchmap = map(document.documentElement.scrollTop, 0, twitchscroll.scrollHeight, 0, 150);
+  twitchcounter.textContent = round(twitchmap);
+if (round(twitchmap) > 150) {
+  twitchcounter.textContent = 150;
+}
+}
 
+function updateNetflixCounter() {
+  'use strict';
+  netflixmap = map(document.documentElement.scrollTop, 0, netflixscroll.scrollHeight, 0, 300);
+  netflixcounter.textContent = round(netflixmap);
+if (round(netflixmap) > 300) {
+  netflixcounter.textContent = 300;
+}
+}
+
+function updateYouTubeCounter() {
+  'use strict';
+  youtubemap = map(document.documentElement.scrollTop, 0, youtubescroll.scrollHeight, 0, 1500);
+  youtubecounter.textContent = round(youtubemap);
+if (round(youtubemap) > 1500) {
+  youtubecounter.textContent = 1500;
+}
 }
 
 
 
-document.addEventListener('scroll', updateCounter);
-window.addEventListener('resize', updateCounter);
+document.addEventListener('scroll', updateTwitchCounter);
+document.addEventListener('scroll', updateNetflixCounter);
+document.addEventListener('scroll', updateYouTubeCounter);
+window.addEventListener('resize', updateTwitchCounter);
