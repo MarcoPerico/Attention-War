5b0011781c86d2d36dcad56d18562d39ca0c4596
MarcoPerico <marcoperico.mail@gmail.com>
2021-12-14 01:46:23 +0100

    bastaviprego

--- SearchMarco/script.js
+++ SearchMarco/script.js
@@ -8,30 +8,46 @@ jQuery(document).ready(function( $ ){
                i += 1;
   });
   $(".thumbnail").hover(function highlight(){
+    $(".nothovering").addClass("off");
+$(".hovering").removeClass("off");
     if($(this).hasClass("keyword")){
-$(".keyword").css("outline","1px red solid");
+$(".keyword").css("outline","2px red solid");
+$("#category1").removeClass("off");
+$(".hovering").css("color","red");
 $(".thumbnail").not(".keyword").css("opacity","0.3");
   }else if($(this).hasClass("searchrelated")){
-    $(".searchrelated").css("outline","1px blue solid");
+    $(".hovering").css("color","blue");
+    $("#category2").removeClass("off");
+    $(".searchrelated").css("outline","2px blue solid");
     $(".thumbnail").not(".searchrelated").css("opacity","0.3");
       }else if($(this).hasClass("unrelated")){
-        $(".unrelated").css("outline","1px rgb(0,255,0) solid");
+        $(".hovering").css("color","rgb(0,255,0)");
+        $("#category3").removeClass("off");
+        $(".unrelated").css("outline","2px rgb(0,255,0) solid");
         $(".thumbnail").not(".unrelated").css("opacity","0.3");
           }else if($(this).hasClass("rewatch")){
-            $(".rewatch").css("outline","1px fuchsia solid");
+            $(".hovering").css("color","fuchsia");
+            $("#category4").removeClass("off");
+            $(".rewatch").css("outline","2px fuchsia solid");
             $(".thumbnail").not(".rewatch").css("opacity","0.3");
               }},function norosso(){
+                $(".hovering").addClass("off");
+                $(".nothovering").removeClass("off");
                              if($(this).hasClass("keyword")){
                               $(".keyword").css("outline","none");
+                              $("#category1").addClass("off");
                              $(".thumbnail").not(".keyword").css("opacity","1");
                          }else if($(this).hasClass("searchrelated")){
                           $(".searchrelated").css("outline","none");
+                          $("#category2").addClass("off");
                        $(".thumbnail").not(".searchrelated").css("opacity","1");
                         }else if($(this).hasClass("unrelated")){
                           $(".unrelated").css("outline","none");
+                          $("#category3").addClass("off");
                           $(".thumbnail").not(".unrelated").css("opacity","1");
                             }else if($(this).hasClass("rewatch")){
                               $(".rewatch").css("outline","none");
+                              $("#category4").addClass("off");
                               $(".thumbnail").not(".rewatch").css("opacity","1");
                                 }
   });
