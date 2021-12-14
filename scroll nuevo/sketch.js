3e70679be65a49ac2a410ad89aec353cf7f52730
irenecasano <37075084+irenecasano@users.noreply.github.com>
2021-12-14 14:52:58 +0100

    twitch counter

--- scroll nuevo/sketch.js
+++ scroll nuevo/sketch.js
@@ -20,13 +20,33 @@ var twitchm = document.getElementById("twitch");
 //    counters.textContent= document.documentElement.scrollTop / height * 150;
 // }
 
-function updateCounter() {
-  'use strict';
-  var heightno = twitchm.scrollHeight - twitchm.height;
-   counters.textContent= twitchm.scrollTop / heightno * 150;
+// scrollHeight = pixel counter for an element
+// scrollTop = quanto ho scrollato di quell'elemento
+// heightno = how much scroll i have left
+
+let m;
+
+// function updateCounter() {
+//   'use strict';
+//   var heightno = twitchm.scrollHeight - window.innerHeight;
+//    counters.textContent = document.documentElement.scrollTop / heightno * 150;
+//   //  console.log(heightno);
+//   //  console.log(window.innerHeight)
+//   // console.log(window.innerHeight)
+// }
+
+function draw() {
+  updateCounter()
 }
 
-console.log(twitchm.width);
+function updateCounter() {
+  'use strict';
+  m = map(document.documentElement.scrollTop, 0, twitchm.scrollHeight, 0, 150);
+   counters.textContent = round(m);
+
+}
+
+
 
 document.addEventListener('scroll', updateCounter);
 window.addEventListener('resize', updateCounter);
