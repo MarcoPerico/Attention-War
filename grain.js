d990a3cde6be3b121995cc932b1a2d5c5ede20d0
Lorenzo Bernini <lorenzobernini98@gmail.com>
2021-12-16 01:26:02 +0100

    sas

--- grain.js
+++ grain.js
@@ -1,8 +1,8 @@
 var options = {
   animate: true,
-  patternWidth: 100,
-  patternHeight: 100,
-  grainOpacity: 0.1,
+  patternWidth: 500,
+  patternHeight: 500,
+  grainOpacity: 0.12,
   grainDensity: 1,
   grainWidth: 1,
   grainHeight: 1,
