1cf585ff109265116c21ae94a6e016755dbfc11b
Lorenzo Bernini <lorenzobernini98@gmail.com>
2021-12-17 11:59:42 +0100

    messa home chiara

--- homepage-landing-new/assets/robematter.js
+++ homepage-landing-new/assets/robematter.js
@@ -52,7 +52,7 @@ function Box(source, x, y, w, h) {
     translate(pos.x, pos.y);
     //blendMode(BLEND);
     fill(0);
-    rotate(angle);
+    // rotate(angle);
     // rectMode(CENTER);
     // rect(0, 0, this.w, this.h, 20);
     // fill(255);
