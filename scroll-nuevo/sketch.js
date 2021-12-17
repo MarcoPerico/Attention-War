39866bbe8106c2c02d43a746043adb8e50759a04
Lorenzo Bernini <lorenzobernini98@gmail.com>
2021-12-17 17:16:29 +0100

    cover scroll

--- scroll-nuevo/sketch.js
+++ scroll-nuevo/sketch.js
@@ -1,83 +1,110 @@
-jQuery(document).ready(function( $ ){
-$(document).scroll(function(){
-  var y = $(this).scrollTop();
-  if(y>3000){
-    $("#goon").removeClass("flexioni");
-    $("#goon").css("opacity","1");
-  } else {
-    $("#goon").css("opacity","0");
-    $("#goon").addClass("flexioni");
+jQuery(document).ready(function ($) {
+  //COVER
+  function inizio() {
+    $(".titolo").css("transform", "translate(0, 0)");
+    $(".capitolo").css({ opacity: "100%", transform: "translate(0,-7px)" });
+    $(".info").css({ opacity: "100%", transform: "translate(0,-7px)" });
+    $(".bottone-start").css({
+      opacity: "100%",
+      transform: "translate(0,30px)",
+    });
+    $("#containerone").hide();
   }
-})
-$("#goon").on("click", function spariscizioviatiprego(){
-$("#campotot").css("opacity","0");
-$("#campotot").delay(500).addClass("off");
-$("#goon").css("opacity","0");
-$("#campotot").delay(500).addClass("off");
-$("body").css("overflow-y","hidden");
-$("#campotot2").removeClass("off");
-$('html, body').animate({scrollTop: 0}, 1000);
-});
-$("#boxinoisbacc").on("click",function(){
-  if($(".shown").hasClass("chiuso")){
-    $("#daytonakk1").css({"height":"2vh","width":"2vh"});
-    $("#netflixshown").css("height","0.875%");
-    $("#primeshown").css("height","19.125%");
-    $(".shown").children().css("opacity","1");
-    $("#youtubeshown").css("height","7.825%");
-    $("#twitchshown").css("height","3.475%");
-    $(".shown").removeClass("chiuso");
-  } else{
-    $("#netflixshown,#primeshown, #youtubeshown, #twitchshown").css("height","0%");
-    $(".shown").addClass("chiuso");
-    $(".shown").children().css("opacity","0");
-    $("#daytonakk1").css({"height":"0","width":"0"});
-  }
-});
-$("#boxinoisbacc2").on("click",function(){
-  if($(".scroll").hasClass("chiuso")){
-    $("#netflixscroll").css("height","52.175%");
-    $("#daytonakk2").css({"height":"2vh","width":"2vh"});
-    $("#primescroll").css("height","33%");
-    $(".scroll").children().css("opacity","1");
-    $("#youtubescroll").css("height","17.3875%");
-    $("#twitchscroll").css("height","24.3375%");
-    $(".scroll").removeClass("chiuso");
-  } else{
-    $("#netflixscroll,#primescroll, #youtubescroll, #twitchscroll").css("height","0%");
-    $(".scroll").addClass("chiuso");
-    $(".scroll").children().css("opacity","0");
-    $("#daytonakk2").css({"height":"0","width":"0"});
-  }
-});
-$("#boxinoisbacc3").on("click",function(){
-  if($(".click").hasClass("chiuso")){
-    $("#netflixclick").css("height","46.95%");
-    $("#daytonakk3").css({"height":"2vh","width":"2vh"});
-    $("#primeclick").css("height","27.825%");
-    $("#youtubeclick").css("height","12.1625%");
-    $(".click").children().css("opacity","1");
-    $("#twitchclick").css("height","6.95%");
-    $(".click").removeClass("chiuso");
-  } else{
-    $("#netflixclick,#primeclick, #youtubeclick, #twitchclick").css("height","0%");
-    $(".click").addClass("chiuso");
-    $(".click").children().css("opacity","0");
-    $("#daytonakk3").css({"height":"0","width":"0"});
-  }
-});
+
+  setTimeout(inizio, 1000);
+
+  $(".bottone-start").on("click", function partiamo() {
+    $("#cover-scroll").fadeOut();
+    $("#containerone").show();
+  });
+
+  //MARCO
+  $(document).scroll(function () {
+    var y = $(this).scrollTop();
+    if (y > 3000) {
+      $("#goon").removeClass("flexioni");
+      $("#goon").css("opacity", "1");
+    } else {
+      $("#goon").css("opacity", "0");
+      $("#goon").addClass("flexioni");
+    }
+  });
+  $("#goon").on("click", function spariscizioviatiprego() {
+    $("#campotot").css("opacity", "0");
+    $("#campotot").delay(500).addClass("off");
+    $("#goon").css("opacity", "0");
+    $("#campotot").delay(500).addClass("off");
+    $("body").css("overflow-y", "hidden");
+    $("#campotot2").removeClass("off");
+    $("html, body").animate({ scrollTop: 0 }, 1000);
+  });
+  $("#boxinoisbacc").on("click", function () {
+    if ($(".shown").hasClass("chiuso")) {
+      $("#daytonakk1").css({ height: "2vh", width: "2vh" });
+      $("#netflixshown").css("height", "0.875%");
+      $("#primeshown").css("height", "19.125%");
+      $(".shown").children().css("opacity", "1");
+      $("#youtubeshown").css("height", "7.825%");
+      $("#twitchshown").css("height", "3.475%");
+      $(".shown").removeClass("chiuso");
+    } else {
+      $("#netflixshown,#primeshown, #youtubeshown, #twitchshown").css(
+        "height",
+        "0%"
+      );
+      $(".shown").addClass("chiuso");
+      $(".shown").children().css("opacity", "0");
+      $("#daytonakk1").css({ height: "0", width: "0" });
+    }
+  });
+  $("#boxinoisbacc2").on("click", function () {
+    if ($(".scroll").hasClass("chiuso")) {
+      $("#netflixscroll").css("height", "52.175%");
+      $("#daytonakk2").css({ height: "2vh", width: "2vh" });
+      $("#primescroll").css("height", "33%");
+      $(".scroll").children().css("opacity", "1");
+      $("#youtubescroll").css("height", "17.3875%");
+      $("#twitchscroll").css("height", "24.3375%");
+      $(".scroll").removeClass("chiuso");
+    } else {
+      $("#netflixscroll,#primescroll, #youtubescroll, #twitchscroll").css(
+        "height",
+        "0%"
+      );
+      $(".scroll").addClass("chiuso");
+      $(".scroll").children().css("opacity", "0");
+      $("#daytonakk2").css({ height: "0", width: "0" });
+    }
+  });
+  $("#boxinoisbacc3").on("click", function () {
+    if ($(".click").hasClass("chiuso")) {
+      $("#netflixclick").css("height", "46.95%");
+      $("#daytonakk3").css({ height: "2vh", width: "2vh" });
+      $("#primeclick").css("height", "27.825%");
+      $("#youtubeclick").css("height", "12.1625%");
+      $(".click").children().css("opacity", "1");
+      $("#twitchclick").css("height", "6.95%");
+      $(".click").removeClass("chiuso");
+    } else {
+      $("#netflixclick,#primeclick, #youtubeclick, #twitchclick").css(
+        "height",
+        "0%"
+      );
+      $(".click").addClass("chiuso");
+      $(".click").children().css("opacity", "0");
+      $("#daytonakk3").css({ height: "0", width: "0" });
+    }
+  });
 });
 function cl(text) {
   console.log(text);
-};
+}
 
 window.addEventListener("scroll", () => {
-  
   const span = document.querySelector("span");
   span.innerHTML = Math.round(window.scrollY);
-  
+
   var wScroll = window.scrollY;
-  
 });
 
 let twitchcounter = document.getElementById("tspan");
@@ -117,49 +144,73 @@ let primemap;
 // }
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
-}
+};
 function draw() {
-  updateTwitchCounter()
-  updateNetflixCounter()
-  updateYouTubeCounter()
-  updatePrimeCounter()
+  updateTwitchCounter();
+  updateNetflixCounter();
+  updateYouTubeCounter();
+  updatePrimeCounter();
 }
 function updateTwitchCounter() {
-  'use strict';
-  twitchmap = map(document.documentElement.scrollTop, 0, twitchscroll.scrollHeight, 0, 73);
+  "use strict";
+  twitchmap = map(
+    document.documentElement.scrollTop,
+    0,
+    twitchscroll.scrollHeight,
+    0,
+    73
+  );
   twitchcounter.textContent = round(twitchmap);
-if (round(twitchmap) > 73) {
-  twitchcounter.textContent = 73;
-}
+  if (round(twitchmap) > 73) {
+    twitchcounter.textContent = 73;
+  }
 }
 
 function updateNetflixCounter() {
-  'use strict';
-  netflixmap = map(document.documentElement.scrollTop, 0, netflixscroll.scrollHeight, 0, 235);
+  "use strict";
+  netflixmap = map(
+    document.documentElement.scrollTop,
+    0,
+    netflixscroll.scrollHeight,
+    0,
+    235
+  );
   netflixcounter.textContent = round(netflixmap);
-if (round(netflixmap) > 235) {
-  netflixcounter.textContent = 235;
-}
+  if (round(netflixmap) > 235) {
+    netflixcounter.textContent = 235;
+  }
 }
 
 function updateYouTubeCounter() {
-  'use strict';
-  youtubemap = map(document.documentElement.scrollTop, 0, youtubescroll.scrollHeight, 0, 881);
+  "use strict";
+  youtubemap = map(
+    document.documentElement.scrollTop,
+    0,
+    youtubescroll.scrollHeight,
+    0,
+    881
+  );
   youtubecounter.textContent = round(youtubemap);
-if (round(youtubemap) > 881) {
-  youtubecounter.textContent = 881;
-}
+  if (round(youtubemap) > 881) {
+    youtubecounter.textContent = 881;
+  }
 }
 function updatePrimeCounter() {
-  'use strict';
-  primemap = map(document.documentElement.scrollTop, 0, primescroll.scrollHeight, 0, 433);
+  "use strict";
+  primemap = map(
+    document.documentElement.scrollTop,
+    0,
+    primescroll.scrollHeight,
+    0,
+    433
+  );
   primecounter.textContent = round(primemap);
-if (round(primemap) > 433) {
-  primecounter.textContent = 433;
-}
+  if (round(primemap) > 433) {
+    primecounter.textContent = 433;
+  }
 }
 
-document.addEventListener('scroll', updateTwitchCounter);
-document.addEventListener('scroll', updateNetflixCounter);
-document.addEventListener('scroll', updateYouTubeCounter);
-document.addEventListener('scroll', updatePrimeCounter);
+document.addEventListener("scroll", updateTwitchCounter);
+document.addEventListener("scroll", updateNetflixCounter);
+document.addEventListener("scroll", updateYouTubeCounter);
+document.addEventListener("scroll", updatePrimeCounter);
